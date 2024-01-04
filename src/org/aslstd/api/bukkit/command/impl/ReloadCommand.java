b81659d501eac402b936854f7375eea100415d95
Snoop1CattZ69 <agronaut8323@gmail.com>
2024-01-04 22:35:18 +0200

    ReloadCommand now uses OpenPlugin class directly

--- src/org/aslstd/api/bukkit/command/impl/ReloadCommand.java
+++ src/org/aslstd/api/bukkit/command/impl/ReloadCommand.java
@@ -5,25 +5,22 @@ import java.lang.reflect.Method;
 
 import org.aslstd.api.bukkit.command.SenderType;
 import org.aslstd.api.bukkit.message.Texts;
+import org.aslstd.api.openlib.plugin.OpenPlugin;
 
 class ReloadCommand extends CommandNode {
-
-	private static Class<?> openPluginClass;
 	private static Method reloadPlugin;
 
 	static {
 		try {
-			openPluginClass = Class.forName("org.aslstd.api.openlib.plugin.OpenPlugin");
-			reloadPlugin = openPluginClass.getDeclaredMethod("reloadPlugin");
-		} catch (final ClassNotFoundException | NoSuchMethodException | SecurityException e) {
-			openPluginClass = null;
+			reloadPlugin = OpenPlugin.class.getDeclaredMethod("reloadPlugin");
+		} catch (final NoSuchMethodException | SecurityException e) {
 			Texts.warn("OpenPlugin class not finded, maybe you has installed an built-in version of this API. Default reload command cannot be initialised.");
 		}
 	}
 
 	public ReloadCommand(CommandHandler handler) {
 		super(handler, "reload", 0, (s,args) -> {
-			if (openPluginClass.isAssignableFrom(handler.plugin.getClass())) {
+			if (OpenPlugin.class.isAssignableFrom(handler.plugin.getClass())) {
 				try {
 					reloadPlugin.invoke(handler.plugin);
 				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
